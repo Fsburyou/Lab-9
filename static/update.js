f737dbf575a6cc29cfc5f06b6d98f529a86beb4f
Viktoria Burdasova <burwhykoroche@gmail.com>
2025-06-25 20:58:19 +0300

    лаба сделана ура

--- static/update.js
+++ static/update.js
@@ -8,6 +8,27 @@ function updateProd(el) {
     console.log(product_id)
 }
 
+function clearBooks() {
+    fetch('/clear', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            const bookList = document.getElementById('book-list');
+            bookList.innerHTML = '';
+        } else {
+            alert(data.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
 function addProduct() {
     let prodName = document.getElementById('prod_name').value
     let price = document.getElementById('price').value
@@ -18,5 +39,4 @@ function addProduct() {
                              'price': price,
                              'in_stock': true})
     })
-//    console.log("Add")
 }
